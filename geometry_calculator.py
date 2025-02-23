e3c1eeb4e7d54881220f6deb7787888cc12e5c7a
Sagar Patel <patelsagar.developer@gmail.com>
2025-02-23 16:16:34 +0530

    Implemented circle area calculation

--- geometry_calculator.py
+++ geometry_calculator.py
@@ -11,3 +11,6 @@ class GeometryCalculator:
 if __name__ == "__main__":
 
     calculator = GeometryCalculator()
+
+    radius = 5
+    print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
